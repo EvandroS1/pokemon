9c07c269d7784a085c119cd483c99a58e37a36f5
Evandro <evandro.gomes@actionsys.com.br>
2024-06-15 14:43:42 -0300

    Projeto atualizado e mostrando os pokemons.

---
+++ app/src/main/java/br/com/fabricadesinapse/pokedex_android/viewmodel/PokemonViewModelFactory.kt
@@ -0,0 +1,15 @@
+package br.com.fabricadesinapse.pokedex_android.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+@Suppress("UNCHECKED_CAST")
+class PokemonViewModelFactory : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PokemonViewModel::class.java)) {
+            return PokemonViewModel() as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
